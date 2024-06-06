4dd6d0817a68bbdebd177a17dda9bed467d1e45c
Wood <vladikvlasov@yandex.ru>
2024-06-06 17:28:43 +0400

    add hw5

---
+++ hw5_VlasoW_Task_1.js
@@ -0,0 +1,19 @@
+//1
+
+for (let i = 10; i >= 0; i-- ) {
+    console.log(i)
+}
+
+//2
+for (let i = 1; i <= 5; i++) {
+    console.log(":)".repeat(i))
+}
+
+// через while
+
+let i = 1
+
+while(i <= 5){
+    console.log(":)".repeat(i))
+    i++
+}
